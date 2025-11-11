5d7a742cb7f82de09e52f89cf93e8a3707a96805
Danny <kilatomerat@gmail.com>
2025-11-11 12:22:38 +0200

    Initial commit

---
+++ app/lib/utils.ts
@@ -0,0 +1,14 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function getApiUrl(path: string) {
+    const baseUrl = import.meta.env.VITE_API_BASE_URL
+    if (!baseUrl) {
+        throw new Error('VITE_API_BASE_URL is not defined')
+    }
+    return `${baseUrl}${path}`
+}
